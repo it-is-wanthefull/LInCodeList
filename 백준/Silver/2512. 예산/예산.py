bc148958dcc02f86f95e469b5f34264def18386f
it-is-wanthefull <itiswanthefull@gmail.com>
2024-02-24 10:40:55 +0900

    [Silver II] Title: 예산, Time: 44 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -1,24 +1,22 @@
 n = int(input())
-lst = list(map(int, input().split()))
+lst = sorted(list(map(int, input().split())))
 m = int(input())
 
-lst.sort()
 if sum(lst) <= m:
     print(max(lst))
 else:
-    total_divide = 0
+    divide_n = 0
     exceed = -1
     pass_mem = 0
     while exceed != 0:
-        divide_n = m // (n-pass_mem)
+        divide_n += (m // (n-pass_mem))
         m = m % (n-pass_mem)
-        total_divide += divide_n
         exceed = 0
         for i in range(pass_mem, n):
-            if lst[i] <= total_divide:
-                exceed += total_divide - lst[i]
+            if lst[i] <= divide_n:
+                exceed += divide_n - lst[i]
             else:
                 m += exceed
                 pass_mem = i
                 break
-    print(total_divide)
+    print(divide_n)
