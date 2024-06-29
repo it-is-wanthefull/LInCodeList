85c5212f80c2786b28d3b24bfb432bb4b1d4f3e7
chan <itiswanthefull@gmail.com>
2024-06-29 15:16:38 +0900

    [level 0] Title: 옹알이 (1), Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -1,13 +1,11 @@
 def solution(babbling):
+    substring_list = ["aya", "ye", "woo", "ma", "****", "***", "**"]
     answer_count = 0
-    substring_list = ["aya", "ye", "woo", "ma"]
-    zip_list = ["****", "***", "**", "*"]
     
     for string in babbling:
         for substring in substring_list:
             string = string.replace(substring, "*")
-        for e in zip_list:
-            string = string.replace(e, "count")
-        if string == "count":
+        if string == "*":
             answer_count += 1
+            
     return answer_count
