e6f3f4257cb6eea3862a3bb6b2fbc392bf96533d
chan <itiswanthefull@gmail.com>
2024-08-06 15:10:15 +0900

    [level 3] Title: 정수 삼각형, Time: 29.42 ms, Memory: 14.6 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,8 +1,49 @@
 def solution(triangle):
-    for i in range(1, len(triangle)):
-        triangle[i][0] += triangle[i-1][0]
-        triangle[i][-1] += triangle[i-1][-1]
-        for j in range(1, len(triangle[i])-1):
-            triangle[i][j] += max( triangle[i-1][j-1], triangle[i-1][j] )
+    for i in range(len(triangle) -1):
+        triangle[i+1][0] += triangle[i][0]
+        triangle[i+1][-1] += triangle[i][-1]
         
+        for j in range(len(triangle[i+1]) -2):
+            if triangle[i][j] > triangle[i][j+1]:
+                triangle[i+1][j+1] += triangle[i][j]
+            else: 
+                triangle[i+1][j+1] += triangle[i][j+1]
+                
     return max(triangle[-1])
+
+
+
+
+
+# def solution(triangle):
+#     for i in range(len(triangle) -1):
+#         triangle[i+1][0] += triangle[i][0]
+#         triangle[i+1][-1] += triangle[i][-1]
+        
+#         for j in range(len(triangle[i+1]) -2):
+#             if triangle[i][j] > triangle[i][j+1]:
+#                 triangle[i+1][j+1] += triangle[i][j]
+#             else: 
+#                 triangle[i+1][j+1] += triangle[i][j+1]
+                
+#     return max(triangle[-1])
+
+
+
+
+
+# def solution(triangle):
+#     for height in range(len(triangle),1,-1):
+#         for j in range(height-1):
+#             if triangle[height-1][j] > triangle[height-1][j+1]:
+#                 triangle[height-2][j] += triangle[height-1][j]
+#             else:
+#                 triangle[height-2][j] += triangle[height-1][j+1]
+        
+#     return triangle[0][0]
+
+
+
+
+
+# solution = lambda t, l = []: max(l) if not t else solution(t[1:], [max(x,y)+z for x,y,z in zip([0]+l, l+[0], t[0])])
