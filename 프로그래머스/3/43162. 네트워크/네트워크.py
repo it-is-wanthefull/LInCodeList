00ff86abb04da62c51d122846e33c3a938c1ab17
chan <itiswanthefull@gmail.com>
2024-08-07 12:15:32 +0900

    [level 3] Title: 네트워크, Time: 1.86 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,7 +1,7 @@
 def solution(n, computers):
     network = 0
     visited = [0] * n
-    schedule = [0]
+    schedule = []
     
     for node in range(n):
         if visited[node] == 1:
@@ -21,3 +21,44 @@ def solution(n, computers):
         network += 1
         
     return network
+
+
+
+
+
+# def solution(n, computers):
+#     answer = 0
+#     visited = [0 for i in range(n)]
+#     def dfs(computers, visited, start):
+#         stack = [start]
+#         while stack:
+#             j = stack.pop()
+#             if visited[j] == 0:
+#                 visited[j] = 1
+#             # for i in range(len(computers)-1, -1, -1):
+#             for i in range(0, len(computers)):
+#                 if computers[j][i] ==1 and visited[i] == 0:
+#                     stack.append(i)
+#     i=0
+#     while 0 in visited:
+#         if visited[i] ==0:
+#             dfs(computers, visited, i)
+#             answer +=1
+#         i+=1
+#     return answer
+
+
+
+
+
+# def solution(n, computers):
+#     temp = []
+#     for i in range(n):
+#         temp.append(i)
+#     for i in range(n):
+#         for j in range(n):
+#             if computers[i][j]:
+#                 for k in range(n):
+#                     if temp[k] == temp[i]:
+#                         temp[k] = temp[j]
+#     return len(set(temp))
