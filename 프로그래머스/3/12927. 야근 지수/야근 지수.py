d8fc50e137b75a1cc2662a263c7c8af77fe995b4
chan <itiswanthefull@gmail.com>
2024-08-07 17:08:37 +0900

    [level 3] Title: 야근 지수, Time: 2.93 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -31,31 +31,31 @@
             
             
             
-# from collections import Counter
+from collections import Counter
 
-# def solution(n, works):
-#     if sum(works) <= n:
-#         return 0
+def solution(n, works):
+    if sum(works) <= n:
+        return 0
     
-#     times = sorted(Counter(works).items(), reverse=True)
+    times = sorted(Counter(works).items(), reverse=True)
     
-#     while True:
-#         hour, count = times[0]
-#         if n > count:
-#             n -= count
-#             if len(times)>1 and hour-1 == times[1][0]:
-#                 times[1] = (times[1][0], times[1][1]+count)
-#                 times.pop(0)
-#             else:
-#                 times[0] = (hour-1, count)
-#         else:
-#             return sum(k**2*v for k, v in times) + n*(1-2*hour)
-#             # -hour**2*n +(hour-1)**2*n
-#             # == -nx^2 + n(x-1)^2
-#             # == n(x-1)^2 - nx^2
-#             # == n * ((x-1)^2-x^2)
-#             # == n * (1-2x)
-#             # == n * (1-2*hour)
+    while True:
+        hour, count = times[0]
+        if n > count:
+            n -= count
+            if len(times)>1 and hour-1 == times[1][0]:
+                times[1] = (times[1][0], times[1][1]+count)
+                times.pop(0)
+            else:
+                times[0] = (hour-1, count)
+        else:
+            return sum(k*k*v for k, v in times) + n*(1-2*hour)
+            # -hour**2*n +(hour-1)**2*n
+            # == -nx^2 + n(x-1)^2
+            # == n(x-1)^2 - nx^2
+            # == n * ((x-1)^2-x^2)
+            # == n * (1-2x)
+            # == n * (1-2*hour)
 
 
 
@@ -64,20 +64,29 @@
 # def solution(n, works):
 #     if n>=sum(works):
 #         return 0;
-
 #     while n > 0:
 #         works[works.index(max(works))] -= 1
 #         n -= 1
-    
 #     return sum([w ** 2 for w in works])
 
 
 
 
 
-from heapq import heapify, heappush, heappop
-def solution(n, works):
-    heapify(works := [-i for i in works])
-    for i in range(min(n, abs(sum(works)))):
-        heappush(works, heappop(works)+1)
-    return sum([i*i for i in works])
+# from heapq import heapify, heappush, heappop
+# def solution(n, works):
+#     heapify(works := [-i for i in works])
+#     for i in range(min(n, abs(sum(works)))):
+#         heappush(works, heappop(works)+1)
+#     return sum([i*i for i in works])
+
+
+
+
+
+# def solution(n, works):
+#     while n>=1:
+#         works.sort()
+#         works[-1]-=1
+#         n-=1
+#     return sum(x**2 for x in works)
