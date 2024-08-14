006515b06b43511147a3d487e69fd547d5356bc4
chan <itiswanthefull@gmail.com>
2024-08-14 17:23:03 +0900

    [level 4] Title: 호텔 방 배정, Time: 452.92 ms, Memory: 52.7 MB -BaekjoonHub

--- 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.py
+++ 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.py
@@ -112,7 +112,7 @@ def solution(k, room_number):
             n = dic[n] # 방에 기록된 이정표만큼 건너뛰기형 탐색
             visited.append(n) # 방문할 방 저장
             
-        for v in visited: # 방문한 방의 이정표 업데이트
+        for v in visited: # 방문한 방의 이정표 업뎃
             dic[v] = n + 1
             
         answer.append(n)
