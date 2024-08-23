f6066095fe6077d373f5bb401806a3b60c8ed406
chan <itiswanthefull@gmail.com>
2024-08-24 00:39:56 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,9 +1,23 @@
 function solution(brown, yellow) {
-    function quadratic_formula(a, b) {
-        root_bb_4ac = Math.sqrt(Math.pow(a/2 -2, 2) - 4*b)
-        t1 = (a/2 -2 + root_bb_4ac) /2
-        t2 = (a/2 -2 - root_bb_4ac) /2
-        return [t1+2, t2+2]
+    function quadratic_formula(brown, yellow) {
+        // brown = width*2 + height*2 - 4
+        // yellow = (width-2) * (height-2) = wh - 2(w+h) + 4 = wh - brown
+        
+        // w + h = brown/2 + 2
+        // w * h = brown + yellow
+        
+        // t^2 - (-1)(w+h)t + wh = t^2 - (-1)(brown/2+2) + (brown+yellow)
+        
+        a = 1
+        b = - (brown/2 + 2)
+        c = brown + yellow
+        
+        root_bb_4ac = Math.sqrt(Math.pow(b, 2) - 4*a*c)
+        
+        t1 = (-b + root_bb_4ac) / 2*a
+        t2 = (-b - root_bb_4ac) / 2*a
+        
+        return [t1, t2]
     }
     return quadratic_formula(brown, yellow);
 }
