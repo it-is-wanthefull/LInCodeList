678e11e6f79e58855275964e2bef98c4b26b3f2a
chan <itiswanthefull@gmail.com>
2024-08-24 10:33:04 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.05 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,10 +1,8 @@
 function solution(n) {
     ans = 0;
     
-    while (n) {
-        if (n%2 == 0) {
-            n /= 2;
-        } else {
+    for (; n; n/=2) {
+        if (n%2 == 1) {
             n -= 1;
             ans++;
         }
