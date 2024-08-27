37afc66aa484df28d136394ed3f51e6c7226d403
chan <itiswanthefull@gmail.com>
2024-08-27 18:40:10 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 0.32 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.py
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.py
@@ -1,18 +1,15 @@
-# 내 풀이 1: 브루트포스 방식
+# # 내 풀이 1: 브루트포스 방식
 # def solution(n):
-#     answer = [1,1,2,5,14,42,132,429,1430,4862,16796]
 #     dp = [['']]
     
-#     for _ in range(n:=10):
-#         prev = dp[-1]
+#     for _ in range(n):
 #         cur = set()
         
-#         for p in prev:
-#             for j in range(len(prev)+1):
-#                 cur.add(p[:j] + '()' + p[j:])
+#         for d in dp[-1]:
+#             for j in range(len(d)+1):
+#                 cur.add(d[:j] + '()' + d[j:])
         
 #         dp.append(list(cur))
-#         print(len(dp[-1]))
         
 #     return len(dp[n])
 
@@ -20,22 +17,20 @@
 
 
 
-# 내 풀이 2: 부분집합(sub-set)의 조합(combinations) 방식
+# # 내 풀이 2: 부분집합(sub-set)의 조합(combinations) 직접 방식
 # from itertools import combinations
 # def solution(n):
 #     dp = [['']]
     
-#     for i in range(1,n+1):
+#     for i in range(1, n+1):
 #         cur = set()
-#         for j in range(i):
 
+#         for j in range(i):
 #             for l in dp[j]:
 #                 for r in dp[i-j-1]:
-#                     tmp = set(combinations([l,r,'(',')'], 4))
-#                     cur.update(tmp)
+#                     cur.update(combinations([l,r,'(',')'], 4))
 
 #         dp.append(list(cur))
-#         print(len(dp[-1]))
         
 #     return len(dp[n])
 
@@ -43,6 +38,31 @@
 
 
 
-# n <= 14 임을 겨냥한 방식
+# 내 풀이 3: 부분집합(sub-set)의 조합(combinations) 수치계산 방식
+from itertools import combinations
 def solution(n):
-    return [1,1,2,5,14,42,132,429,1430,4862,16796,58786,208012,742900,2674440][n]
+    dp = [1,1,2,5]
+    
+    for i in range(len(dp), n+1):
+        dp.append(0)
+        for j in range(i):
+            dp[-1] += dp[j] * dp[i-j-1]
+            
+    return dp[n]
+
+
+
+
+
+# # 내 풀이 4: n <= 14 임을 겨냥한 방식
+# def solution(n):
+#     return [1,1,2,5,14,42,132,429,1430,4862,16796,58786,208012,742900,2674440][n]
+
+
+
+
+
+# # 베스트채택된 다른사람 풀이 1: 카탈란 수
+# from math import factorial as f
+# def solution(n):
+#     return f(2*n)//(f(n)**2*(n+1))
