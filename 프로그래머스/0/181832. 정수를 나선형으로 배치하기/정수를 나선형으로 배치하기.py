9d0788401f07029cbc8330c0a73af4e8c428f843
chan <itiswanthefull@gmail.com>
2024-09-06 18:22:04 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.17 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
@@ -1,36 +1,17 @@
 def solution(n):
-    answer = [[0] * n for _ in range(n)]  #answer = [[]] 은 틀린 2차원배열 선언
-    r, c, cnt = 0, 0-1, 1-1
+    answer = [[0] * n for _ in range(n)]    # answer = [[]] 은 틀린 2차원배열 선언
+    r, c, cnt, sign = 0, -1, 0, 1           # sign 은 부호를 뜻함
     
-    for j0 in range(n):
-            r, c, cnt = r, c+1, cnt+1
-            answer[r][c] = cnt
-    
-    n -= 1
-    
-    while(1):
-        if n == 0:
-            break;
-        
-        for j1 in range(n):
-            r, c, cnt = r+1, c, cnt+1
-            answer[r][c] = cnt
-        for j2 in range(n):
-            r, c, cnt = r, c-1, cnt+1
+    while n > 0:
+        for _ in range(n):
+            r, c, cnt = r, c+sign, cnt+1
             answer[r][c] = cnt
         
+        for _ in range(n-1):
+            r, c, cnt = r+sign, c, cnt+1
+            answer[r][c] = cnt
+        
+        sign *= -1
         n -= 1
-        
-        if n == 0:
-            break;
-        
-        for j3 in range(n):
-            r, c, cnt = r-1, c, cnt+1
-            answer[r][c] = cnt
-        for j4 in range(n):
-            r, c, cnt = r, c+1, cnt+1
-            answer[r][c] = cnt
-        
-        n -= 1
-        
+            
     return answer
