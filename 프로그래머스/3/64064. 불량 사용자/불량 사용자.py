9153497ec6f161440f8cd5b9c6480e4abbaaa582
chan <itiswanthefull@gmail.com>
2024-10-28 15:02:57 +0900

    [level 3] Title: 불량 사용자, Time: 115.94 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -0,0 +1,38 @@
+import re
+from itertools import product
+
+def solution(user_id, banned_id):
+    outer_list = list()
+    for b in banned_id:
+        
+        inner_list = list()
+        for u in user_id:
+            query = b.replace('*', '.')
+            match = re.match(f'^{query}$', u)
+            if match:
+                inner_list.append(u)
+                
+        outer_list.append(inner_list)
+        
+    # 8의8승=16초, 8팩토리얼=0.04초
+#     for p in product(*outer_list):
+#         if len(set(p)) == len(p):
+#             final_set.add(tuple(sorted(p)))
+            
+#     return len(final_set)
+    
+    def combination_of_list(outer_list, selected_list, final_set):
+        if len(outer_list) == 0:
+            final_set.add(tuple(sorted(selected_list)))
+            return
+        
+        for selected in outer_list[0]:
+            if selected not in selected_list:
+                new_selected_list = selected_list + [selected]
+                combination_of_list(outer_list[1:], new_selected_list, final_set)
+                
+    selected_list = list()
+    final_set = set()
+    combination_of_list(outer_list, selected_list, final_set)
+    
+    return len(final_set)
