76815954d80b922c47888a33400ba2a3f49e30bf
chan <itiswanthefull@gmail.com>
2024-11-05 10:05:44 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 481.08 ms, Memory: 41 MB -BaekjoonHub

---
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+# 서브함수: 3개의 수 중 가운데 수'b'의 절대값'abs()'이 가장작은경우 True 반환
+def is_mid_small(a, b, c):
+    if abs(a) >= abs(b) <= abs(c):  return True
+    else:                           return False
+
+# 메인함수
+def solution(sequence):
+    # 1) 홀수때 -1 펄스 반영한 버전을 세팅 (짝수는 단순 반전일뿐) = O(n)
+    s2 = deque([])
+    for i in range(len(sequence)):
+        if i % 2 == 1:             s2.append( -sequence[i] ) # 짝수때 -1펄스
+        else:                      s2.append(  sequence[i] ) # 홀수때 +1펄스
+    
+    # 2) 음수/양수끼리 묶어 합 = O(n)
+    s3 = deque([s2.popleft()])
+    while len(s2)>=1:
+        if s3[-1] * s2[0] >= 0:    s3[-1] +=  s2.popleft()   # 부호가 같으면
+        else:                      s3.append( s2.popleft() ) # 부호가 다르면
+    
+    # 3) 연속한3개의 수 중 가운데 수의 절대값이 가장작은경우 추가 묶기 반복 = O(n)
+    s4 = deque([s3.popleft()])
+    while len(s3)>=1:
+        if    len(s3)>=2 and is_mid_small(s4[-1], s3[ 0], s3[ 1]):
+            s4.append(s4.pop()     + s3.popleft() + s3.popleft())
+        else:
+            s4.append(s3.popleft())
+        while len(s4)>=3 and is_mid_small(s4[-3], s4[-2], s4[-1]):
+            s4.append(s4.pop()     + s4.pop()     + s4.pop()    )
+    
+    return max(max(s4), -min(s4)) # '-min'은 반전버전을 고려한 것
